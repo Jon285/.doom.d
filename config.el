ddd470a6387bc89e05e8ba33e662a580987c5761
Jonatas <jbmagalhaes28@gmail.com>
2019-08-09 17:12:55 -0300

    Workflow config complete

--- config.el
+++ config.el
@@ -4,6 +4,13 @@
 (load-theme 'doom-city-lights t)
 (require 'origami)
 (global-origami-mode)
+(company-quickhelp-mode)
+
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'rust-mode-hook 'rust-mode)
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
 
 ;; Keybindings
 (with-eval-after-load 'evil-maps
@@ -32,21 +39,30 @@
     (cl-loop for i from 1 to 5
              do (evil-next-visual-line)))
   (map!
-   :ni "<f12>" #'treemacs
-   :ni "<f2>"  #'evil-save
-   :ni "<f4>"  #'comment-line
-   :niv "C-k"  #'move-line-up
-   :niv "C-j"  #'move-line-down
-   :nv "H"     #'evil-first-non-blank-of-visual-line
-   :nv "J"     #'next-5-lines
-   :nv "K"     #'previous-5-lines
-   :nv "L"     #'evil-end-of-visual-line
-   :n "O"      #'origami-open-node-recursively
-   :n "C"      #'origami-close-node-recursively
-   :n "o"      #'origami-toggle-node))
+   :ni "<f12>"         #'treemacs
+   :ni "<f2>"          #'evil-save
+   :ni "<f4>"          #'comment-line
+   :niv "C-k"          #'move-line-up
+   :niv "C-j"          #'move-line-down
+   :nv "H"             #'evil-first-non-blank-of-visual-line
+   :nv "J"             #'next-5-lines
+   :nv "K"             #'previous-5-lines
+   :nv "L"             #'evil-end-of-visual-line
+   :n "SPC o o o"      #'origami-toggle-node
+   :n "SPC o o O"      #'origami-open-node-recursively
+   :n "SPC o o c"      #'origami-close-node
+   :n "SPC o o C"      #'origami-close-node-recursively
+   :n "SPC r c c"      #'cargo-process-build
+   :n "SPC r c r"      #'cargo-process-run
+   :n "SPC r c d"      #'cargo-process-doc))
 
 ;; General config
 (setq
  doom-font (font-spec :family "Source Code Pro" :size 13)
  doom-big-font (font-spec :family "Source Code Pro" :size 36)
- projectile-project-search-path '("~/Projetos/" "~/Documentos/Rust/engine/" "~/Documentos/Rust/math/"))
+ rust-enable-format-on-save t
+ projectile-project-search-path '("~/Projetos/" "~/Documentos/Rust/engine/" "~/Documentos/Rust/math/")
+
+ c-default-style '((java-mode . "java")
+                   (awk-mode . "awk")
+                   (other . "linux")))
--- packages.el
+++ packages.el
@@ -6,3 +6,8 @@
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 (package! origami)
+(package! company-quickhelp)
+(package! company-irony)
+(package! irony)
+(package! rust-mode)
+(package! cargo)
